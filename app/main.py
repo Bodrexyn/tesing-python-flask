e13abe5b263ef09cfb026eeccd64bca75ab16c8d
ilham-ma <ilhamaulana13@gmail.com>
2022-07-12 10:14:11 +0700

    add machine learning

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from modules.model.main import run_model
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/", methods=['POST'])
+def post():
+    response = json.loads(request.data)
+    arrayStr = response['array'].split(',')
+    arrayInt = list(map(int, arrayStr))
+    result = run_model(X_train, X_test, y_train, y_test, arrayInt)
+
+    return str(result)
+
+@app.route("/", methods=['GET'])
+def get():
     return "<p>Hello, World!</p>"
